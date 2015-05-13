76e0efe07e1cffcd884e8532fcb5c346ceb04877
Meghana Kothaluri <kothaluri07@gmail.com>
2015-05-13 12:09:33 -0700

    Modified Controller and some deletions by Meghana Kothaluri

--- src/main/java/facebook/PhotoAttributes.java
+++ src/main/java/facebook/PhotoAttributes.java
@@ -5,56 +5,9 @@ public class PhotoAttributes {
 
 
     //add variables about photos
-    private String photoUrl ;
-    private int numberOfLikes;
-    private int numberOfComments;
+    
     private int numberOfTags;
-    private List<PersonInfo> nameComments;
-    private List<PersonInfo> nameLikes;
-    private List<PersonInfo> nameTags;
-
-    public String getPhotoUrl(){
-        return photoUrl;
-    }
-
-    public void setPhotoUrl(String url){
-        this.photoUrl = url;
-    }
-
-    public List<PersonInfo> getLikes(){
-        return nameLikes;
-    }
-    public List<PersonInfo> getComments(){
-        return nameComments;
-    }
-    public List<PersonInfo> getTags(){
-        return nameTags;
-    }
-
-    public void setLikes(List<PersonInfo> nameList){
-        this.nameLikes = nameList;
-    }
-    public void setComments(List<PersonInfo> nameList){
-        this.nameComments = nameList;
-    }
-    public void setTags(List<PersonInfo> nameList){
-        this.nameTags = nameList;
-    }
-    public int getNumberOfLikes(){
-        return numberOfLikes;
-    }
-
-    public void setNumberOfLikes(int number){
-        this.numberOfLikes = number;
-    }
-
-    public int getNumberOfComments(){
-        return numberOfComments;
-    }
-
-    public void setNumberOfComments(int value){
-        this.numberOfComments = value;
-    }
+   
     public int getNumberOfTags(){
         return numberOfTags;
     }
--- src/main/java/facebook/PostAttributes.java
+++ src/main/java/facebook/PostAttributes.java
@@ -8,74 +8,9 @@ import java.util.List;
 public class PostAttributes {
 
     //add variables about photos
-    private String type ;
-    private String message;
-    private String owner;
-    private int numberOfLikes;
-    private int numberOfComments;
+   
     private int numberOfTags;
-    private List<PersonInfo> nameComments;
-    private List<PersonInfo> nameLikes;
-    private List<PersonInfo> nameTags;
-
-    public String getType(){
-        return type;
-    }
-
-    public void setType(String type){
-        this.type = type;
-    }
-
-    public String getMessage(){
-        return message;
-    }
-
-    public void setMessage(String message){
-        this.message = message;
-    }
-
-    public String getOwner(){
-        return owner;
-    }
-
-    public void setOwner(String owner){
-        this.owner = owner;
-    }
-
-    public List<PersonInfo> getLikes(){
-        return nameLikes;
-    }
-    public List<PersonInfo> getComments(){
-        return nameComments;
-    }
-    public List<PersonInfo> getTags(){
-        return nameTags;
-    }
-
-    public void setLikes(List<PersonInfo> nameList){
-        this.nameLikes = nameList;
-    }
-    public void setComments(List<PersonInfo> nameList){
-        this.nameComments = nameList;
-    }
-    public void setTags(List<PersonInfo> nameList){
-        this.nameTags = nameList;
-    }
-    public int getNumberOfLikes(){
-        return numberOfLikes;
-    }
-
-    public void setNumberOfLikes(int number){
-        this.numberOfLikes = number;
-    }
-
-    public int getNumberOfComments(){
-        return numberOfComments;
-    }
-
-    public void setNumberOfComments(int value){
-        this.numberOfComments = value;
-    }
+   
     public int getNumberOfTags(){
         return numberOfTags;
     }
